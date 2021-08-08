b8e1367d988fb5a7987e028a518adc83abcf90a8
lihonglei <lihonglei19960201@aliyun.com>
2021-08-08 09:44:35 +0800

    堆排序

---
+++ HelloGit/src/main/java/li/剑指offer/数组/堆排序/Solution.java
@@ -0,0 +1,50 @@
+package li.剑指offer.数组.堆排序;
+
+import java.util.Arrays;
+
+/**
+ * 堆排序
+ *
+ * 思路：就是将无序数组转换成大顶堆，然后将大顶堆数组的第一个元素与最后一个元素交换
+ */
+public class Solution {
+    public static void main(String[] args) {
+        int[] array = {8,9,5,4,1,1,4,5,2,4,-1,-999};
+        dumpSort(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+    private static void dumpSort(int[] array){
+        for (int k = array.length; k > 0; k--){
+            //从第一个非子叶节点开始循环
+            for (int i = k / 2 - 1; i >= 0; i--) {
+                bigDump(array,i,k);
+            }
+            swap(array,0,k - 1);
+        }
+    }
+
+    private static void swap(int[] array, int i, int len){
+        int temp;
+        temp = array[0];
+        array[0] = array[len];
+        array[len] = temp;
+    }
+
+    private static void bigDump(int[] array, int i, int len){
+        int temp = array[i];
+        for (int k = 2 * i + 1; k < len; k = k * 2 + 1){
+            if (k + 1 < len && array[k] < array[k + 1]){
+                k++;
+            }
+
+            if (array[k] > temp){
+                array[i] = array[k];
+                i = k;
+            }else {
+                break;
+            }
+        }
+        array[i] = temp;
+    }
+}
