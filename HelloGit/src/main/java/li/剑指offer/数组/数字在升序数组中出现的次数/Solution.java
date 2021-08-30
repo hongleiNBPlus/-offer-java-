41799c201f2180d907fa4755dc7fd8bb26187922
lihonglei <lihonglei19960201@aliyun.com>
2021-08-30 21:03:18 +0800

    有序数组中目标值的个数-二分法

--- HelloGit/src/main/java/li/剑指offer/数组/数字在升序数组中出现的次数/Solution.java
+++ HelloGit/src/main/java/li/剑指offer/数组/数字在升序数组中出现的次数/Solution.java
@@ -14,6 +14,12 @@ public class Solution {
         return getTargetIndex(array,k) - getTargetIndex(array,k - 1);
     }
 
+    /**
+     * 获取目标值的下一个值的索引【1，2，2，2，3】 目标值为2获取返回值最后一个2的索引+1，目标值为1，返回1的索引+1
+     * @param array 目标数组
+     * @param target 目标值
+     * @return
+     */
     int getTargetIndex(int[] array, int target){
         int left = 0, right = array.length - 1;
         int mid;
