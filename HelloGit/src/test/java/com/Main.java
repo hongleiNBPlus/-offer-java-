84237b069a71b09c3be1067c9f1acb656812af7c
lihonglei <lihonglei19960201@aliyun.com>
2021-09-14 23:02:14 +0800

    最长公共子序列

--- HelloGit/src/test/java/com/Main.java
+++ HelloGit/src/test/java/com/Main.java
@@ -5,15 +5,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int ax = sc.nextInt();
-        int ay = sc.nextInt();
-        int bx = sc.nextInt();
-        int by = sc.nextInt();
-        int aAbs = Math.abs(ax - ay);
-        int bAbs = Math.abs(bx - by);
-        int res = Math.abs(aAbs - bAbs);
-        System.out.println(res);
+
     }
 }
 
